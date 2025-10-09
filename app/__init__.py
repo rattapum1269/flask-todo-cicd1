139e87550dd5f3f2cd158b7e61c636ef1be8d0f6
naeem <67030338@kmitl.ac.th>
2025-10-09 19:55:13 +0700

    the endddd

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 limiter = Limiter(
-    app= app,
+    app=app,
     key_func=get_remote_address,
     default_limits=["200 per day", "50 per hour"]
 )
